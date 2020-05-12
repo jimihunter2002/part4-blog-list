30ff79ba4da9edbc8aabcbd83ba5e74a2b1ec815
jimihunter2002 <jimi.hunter008@gmail.com>
2020-05-12 23:02:07 +0100

    uibuild

---
+++ build/precache-manifest.39c909d90d0ed7edaa54f30fcb667b37.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4665348087924be5ccb0a9bca5c0d395",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aca7ade742151ebffe16",
+    "url": "/static/css/main.c0f66210.chunk.css"
+  },
+  {
+    "revision": "62a32e86e12b8c5fc0fc",
+    "url": "/static/js/2.59d7bfca.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.59d7bfca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aca7ade742151ebffe16",
+    "url": "/static/js/main.69ad7209.chunk.js"
+  },
+  {
+    "revision": "a6a2eab1796a366e234b",
+    "url": "/static/js/runtime-main.fd29daab.js"
+  }
+]);
